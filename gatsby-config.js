75333e1db48589c2f9889702a26b3dc653e77a82
Venugopal  K (C) <vgk@tritechsamvit.com>
2023-09-08 07:54:13 +0530

    site updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site GraphQl Test"
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    }
+  ],
 }
